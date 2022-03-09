e3f74e0c8e9e8bf61bc6e5caab28278da7761c2d
SpiralSky <66676886+SpiralSky@users.noreply.github.com>
2022-03-09 12:22:10 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,24 @@
+import os
+import time
+import sys
+import colours
+from colours import *
 
+def printslow(text):
+  for x in text:
+    sys.stdout.write(x)
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+def printfast(text):
+  for x in text:
+    sys.stdout.write(x)
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+def play(text):
+  if text == 1:
+    print("")
+  else:
+    print(Red + "This is not available. Current available difficulties: (1)")
+  
